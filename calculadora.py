de1b229b1c2346eac3c5d0be9d5e5ad3441a9fab
MarianaBraga13 <maribragad@gmail.com>
2025-06-18 14:48:30 -0300

    db.py deletado para focar em versão mais básica

--- calculadora.py
+++ calculadora.py
@@ -1,21 +1,26 @@
 # depósito
+PATRIMONIO = 0.0
 
 def depositar():
+    global PATRIMONIO
     while True:
         deposito = input("Digite o valor para o depósito:\nR$")
         if deposito.isdigit():
             deposito = float(deposito)
             if deposito > 0:
+                PATRIMONIO += deposito
                 print("Depósito recebido com sucesso!")
+                print(f"Seu patrimônio no momento é:R${PATRIMONIO:.2f}")
                 break
             else:
                 print("Digite um valor maior que zero.")
         else:
             print("Digite apenas valores numéricos.")
 
-    return deposito
-        
-             
+    return PATRIMONIO
+
+
+                 
 
             
                      
